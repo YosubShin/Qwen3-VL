58a2b199f0d693600a3ef3207b855691b15ac09d
Yosub Shin <yosub.shin.0@gmail.com>
2025-10-22 23:05:41 -1000

    try again

--- qwen-vl-finetune/scripts/sft_qwen3_4b.sh
+++ qwen-vl-finetune/scripts/sft_qwen3_4b.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 # Distributed training configuration
 MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
@@ -28,42 +29,43 @@ output_dir=./output
 
 # Training arguments
 args="
-    --deepspeed ${deepspeed} \
-    --model_name_or_path ${llm} \
-    --dataset_use ${datasets} \
-    --data_flatten True \
-    --tune_mm_vision False \
-    --tune_mm_mlp True \
-    --tune_mm_llm True \
-    --bf16 \
-    --output_dir ${output_dir} \
-    --num_train_epochs 8 \
-    --per_device_train_batch_size ${batch_size} \
-    --per_device_eval_batch_size $((batch_size*2)) \
-    --gradient_accumulation_steps ${grad_accum_steps} \
-    --max_pixels 50176 \
-    --min_pixels 784 \
-    --evaluation_strategy steps \
-    --eval_steps 40 \
-    --save_strategy steps \
-    --save_steps 40 \
-    --save_total_limit 1 \
-    --load_best_model_at_end True \
-    --metric_for_best_model eval_loss \
-    --greater_is_better False \
-    --learning_rate ${lr} \
-    --weight_decay 0 \
-    --warmup_ratio 0.03 \
-    --max_grad_norm 1 \
-    --lr_scheduler_type cosine \
-    --logging_steps 1 \
-    --model_max_length 8192 \
-    --gradient_checkpointing True \
-    --dataloader_num_workers 4 \
-    --run_name ${run_name} \
+    --deepspeed ${deepspeed}
+    --model_name_or_path ${llm}
+    --dataset_use ${datasets}
+    --data_flatten True
+    --tune_mm_vision False
+    --tune_mm_mlp True
+    --tune_mm_llm True
+    --bf16
+    --output_dir ${output_dir}
+    --num_train_epochs 8
+    --per_device_train_batch_size ${batch_size}
+    --per_device_eval_batch_size $((batch_size*2))
+    --gradient_accumulation_steps ${grad_accum_steps}
+    --max_pixels 50176
+    --min_pixels 784
+    --evaluation_strategy steps
+    --eval_steps 40
+    --save_strategy steps
+    --save_steps 40
+    --save_total_limit 1
+    --load_best_model_at_end True
+    --metric_for_best_model eval_loss
+    --greater_is_better False
+    --learning_rate ${lr}
+    --weight_decay 0
+    --warmup_ratio 0.03
+    --max_grad_norm 1
+    --lr_scheduler_type cosine
+    --logging_steps 1
+    --model_max_length 8192
+    --gradient_checkpointing True
+    --dataloader_num_workers 4
+    --run_name ${run_name}
     --report_to wandb"
 
 # Launch training
+echo "Resolved args: ${args}"
 torchrun --nproc_per_node=${NPROC_PER_NODE} \
          --master_addr=${MASTER_ADDR} \
          --master_port=${MASTER_PORT} \
